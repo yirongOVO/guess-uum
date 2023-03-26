8f3cd599ec8c0454ad93afd0e110f8c8cafd88b9
yirongOVO <youngclaire168@gmail.com>
2023-03-27 01:20:45 +0800

    custom value

--- guess-uum.py
+++ guess-uum.py
@@ -1,9 +1,8 @@
-#產生一個隨機整數1~100
-#讓使用者重複輸入數字去猜
-#猜對了，印出"你答對了"
-#猜錯了，要告訴他比答案大還是小
+
 import random
-r = random.randint(1，100)
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start，end)
 count = 0
 while Ture:
 	count = count + 1
